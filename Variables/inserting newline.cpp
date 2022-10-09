efaaf1678b05d208dd078b66996f0f72779b761b
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-10-09 06:17:31 -0600

    Add files via upload

---
+++ Variables/inserting newline.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// New line can be inserted by adding either "\n" or endl
+	
+	cout<<"-----------Welcome Home----------"<<"\n";
+	cout<<"-----------Congratulations-------"<<endl;
+	cout<<"Good nite";
+	return 0;
+}
