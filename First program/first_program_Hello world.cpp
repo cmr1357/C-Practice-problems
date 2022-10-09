b9d7c66be8ef29d5ca268cae434de21fa96a0f72
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-10-08 18:08:34 -0600

    Add files via upload

---
+++ First program/first_program_Hello world.cpp
@@ -0,0 +1,19 @@
+
+//# include is a header file library which help us to include input and outputs.
+#include <iostream>
+
+//using namespace.std is used for objects and variables from standard library.
+using namespace std;
+
+//int main() is the main function and the code written inside the{} will be executed.
+int main()
+{
+	
+	//cout is an object used together with the insertion operator to print text.
+	cout<<"Hello World!";
+
+	//return 0 ends the main function
+	return 0;
+}
+
+// Note- The blank lines are ignored by the compiler, We are using it to make the code more readable.
