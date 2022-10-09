0558797759d0ee1bf3d12907f0e282f83d85f0d0
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-10-08 18:15:34 -0600

    Add files via upload

---
+++ Variables/Variables_Find_area_of_rectangle_taking_user_nput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int length;
+	int breadth;
+	cout <<"Enter the lengt"<< endl;
+	cin>> length;
+	cout<<"Enter the breadth"<<endl;
+	cin>> breadth;
+	int area=length *breadth;
+	cout<<"Area = " <<area<<endl;
+	return 0;
+}
+
