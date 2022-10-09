1a83945b8bd8162c44c48c3fd79abf867d01caae
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-10-09 06:50:35 -0600

    Add files via upload

---
+++ Variables/find limits of integer.cpp
@@ -0,0 +1,31 @@
+// Upper and lower limits of integers
+
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+int main()
+{
+	cout<<"\n check the upper and lower limits of integer \n";
+	cout<<"-------------------------------------------------- \n";
+	cout<<" The maxium limit of int data type is "<<INT_MAX <<"\n";
+	cout<<" The minimum limit of int data type is "<<INT_MIN <<"\n";
+	cout<<" The maxium limit of unsigned int data type is "<<UINT_MAX <<"\n";
+	cout<<" The maxium limit of int data type is "<<INT_MAX <<"\n";
+	cout<<" The maxium limit of long long data type is "<<LLONG_MAX <<"\n";
+	cout<<" The minimum limit of long long  data type is "<<LLONG_MIN <<"\n";
+	cout<<" The maxium limit of unsigned long long data type is "<<ULLONG_MAX <<"\n";
+	cout<<" The Bits contain in char data type is "<<CHAR_BIT<<"\n";
+	cout<<" The maximum limit of char data type is "<<CHAR_MAX <<"\n";
+	cout<<" The minimum limit of char data type is "<<CHAR_MIN <<"\n";
+	cout<<" The maxium limit of signed char data type is "<<SCHAR_MAX<<"\n";
+	cout<<" The minimum limit of signed char data type is "<<SCHAR_MIN<<"\n";
+	cout<<" The maxium limit of unsigned char data type is "<<UCHAR_MAX <<"\n";
+	
+	cout<<"The maximum limit of short data type is"<<SHRT_MAX<<"\n";
+	cout<<"The minimum limit of short data type is"<<SHRT_MIN<<"\n";
+	
+	
+	
+}
