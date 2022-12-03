7febb487a1c41bd02e3130db16e71254d524cbc3
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-12-03 10:09:23 -0700

    Add files via upload

---
+++ Data Types/Data_types_Practice_Problem1.cpp
@@ -0,0 +1,47 @@
+
+// Data Types
+
+#include<iostream>
+#include<climits>
+#include<string>
+using namespace std;
+
+int main()
+{
+	//Numbers
+	
+	int num=1;
+	cout<<"number=" ;
+	cout<< num<<'\n';
+	float num1=3.14;
+	cout<<num1<<'\n';
+	double  num2=12E4;
+	cout<< num2<<'\n';
+	
+	//Boolean 
+	
+	bool IloveEmbeddedSystems =true;
+	bool Iamavegitarian = false;
+	
+	cout<<IloveEmbeddedSystems<<'\n';
+	cout<< Iamavegitarian<<'\n';
+	return 0;
+	
+	//Characters
+	
+	char favouriteletter = 'A';
+	cout<<favouriteletter;
+	
+	char a=65,b=66,c=67;
+	cout<<a;
+	cout<<b;
+	cout<<c;
+	
+	//string
+	
+	string name="Abhi";
+	
+	cout<<name;
+	
+	
+}
