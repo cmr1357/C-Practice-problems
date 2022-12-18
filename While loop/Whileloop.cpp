16c8fdb8aca84c858a60565017f4aab8c6b13233
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-12-18 00:36:47 -0700

    Add files via upload

---
+++ While loop/Whileloop.cpp
@@ -0,0 +1,19 @@
+//While loop
+
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+int main()
+{
+	// print 1 to 10 using while loop
+	
+	int i=1;
+	while(i<=10)
+	{
+		cout<<i<<'\n';
+		i++;
+	}
+	return 0;
+}
