71e6ddd873b2adc50f88d39a0c2519fff8793bc3
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-12-18 00:38:25 -0700

    Add files via upload

---
+++ For Loop/forloop.cpp
@@ -0,0 +1,20 @@
+//For Loop
+
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+int main()
+{
+ //Print 1 to 10 using for loop
+ 
+ int i=1;
+ 
+ for(i=1;i<=10;i++)
+ {
+ 	cout<<i<<'\n';
+ }
+	 
+	return 0;
+}
