bf227da4c71ccf370ffd43c22beb1c6746b69d77
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-12-18 00:00:08 -0700

    Add files via upload

---
+++ Math/Math_practice_problems.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<climits>
+#include<cmath>// for using log, squareroot..etc we need to use math library.
+
+using namespace std;
+	
+int main()
+{
+	// To find the maximum and minimum
+	
+	cout<<max(5,10)<<'\n';
+	cout<<min(5,10)<<'\n';
+	
+	// To find squareroot, logarithm, round
+	
+	cout<<sqrt(64)<<'\n';
+	cout<<round(3.14)<<'\n';
+	cout<<log(2)<<'\n';	
+	
+	return 0;
+}
